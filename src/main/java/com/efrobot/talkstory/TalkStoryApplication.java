f7ece26cb491e49a8992645b1410ca894c9b79fd
jhz-zhangding <zhangding@ren001.com>
2017-12-18 10:36:34 +0800

    first commit

---
+++ src/main/java/com/efrobot/talkstory/TalkStoryApplication.java
@@ -0,0 +1,16 @@
+package com.efrobot.talkstory;
+
+import android.app.Application;
+
+/**
+ * Created by zd on 2017/12/18.
+ */
+public class TalkStoryApplication extends Application {
+
+    private final String TAG = TalkStoryApplication.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ src/main/java/com/efrobot/talkstory/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.efrobot.talkstory.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.efrobot.talkstory.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
